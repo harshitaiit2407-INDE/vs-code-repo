b271c5574b864489e5f123c241a5c24ac7678f61
Harshit <harshit.aiit2407@gmail.com>
2026-09-24 09:31:46 +0530

    Time: 2 ms (73.1%), Space: 14.8 MB (44.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        for(int i=0; i<nums.size()-1; i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if((nums[i]+nums[j])==target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        unordered_map<int,int> jetha;
+
+        for(int i=0; i<nums.size(); i++){
+            int j=target-nums[i];
+            auto it=jetha.find(j);
+            if(it!=jetha.end()){
+                ans.push_back(i);
+                ans.push_back(jetha[j]);
+                break;
+
             }
-
+            jetha[nums[i]]=i;
         }
-
         return ans;
         
     }
